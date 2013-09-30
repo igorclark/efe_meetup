0961ae66d8fa500a9eb62cdbe12d04951e17576d
Igor Clark <igor@new-host-5.home>
2013-09-29 21:17:30 -0400

    first crack

---
+++ 2-1/geom.erl
@@ -0,0 +1,11 @@
+%% @author Igor Clark <email@address.com>
+%% @doc geometry module implementing function from exercise 2-1 of Etudes for Erlang
+%% @reference <a href="http://chimera.labs.oreilly.com/books/1234000000726/ch02.html">Etudes for Erlang chapter 2</a>
+-module(geom).
+-export([area/2]).
+
+%% @doc Calculates the area of a rectangles using supplied Length and Width arguments.
+%% Returns the area as an integer.
+%% @spec area(integer(), integer()) -> integer()
+area(Length, Width) ->
+	Length * Width.
