0ec537046cfe0e9cbb39b31f79cacdbd9a45a80b
Igor Clark <igor@new-host-5.home>
2013-10-13 19:11:56 -0400

    fixed spec

--- 4-4/powers.erl
+++ 4-4/powers.erl
@@ -6,5 +6,6 @@ raise(_X, 0) -> 1;
 raise(X, N) when N < 0 -> 1.0 / raise(X, -N);
 raise(X, N) when N > 0 -> raise(X, N, 1).
 
+-spec raise(number(), number(), number()) -> number().
 raise(_X, 0, Acc) -> Acc;
 raise(X, N, Acc) -> raise(X, N-1, X*Acc).
